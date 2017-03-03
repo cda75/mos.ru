cad279f3ada1649e90c917d2694b5e81413ce681
cda75 <dchestnov@mail.ru>
2017-03-03 12:11:43 +0300

    edit checking script

--- check_dnevnik.sh
+++ check_dnevnik.sh
@@ -3,7 +3,7 @@
 export DISPLAY=:99
 echo "Start checking diary......"
 /etc/init.d/xvfb start
-/usr/bin/python /home/dimka/mos.ru/dnevnik.py >> /home/dimka/mos.ru/dnevnik.log
+/usr/bin/python /home/dimka/mos.ru/dnevnik.py 
 /etc/init.d/xvfb stop
 echo "Diary successfully checked"
 
--- check_sad.sh
+++ check_sad.sh
@@ -2,6 +2,6 @@
 
 export DISPLAY=:99
 /etc/init.d/xvfb start
-/usr/bin/python /home/dimka/mos.ru/sad.py >> /home/dimka/mos.ru/sad.log
+/usr/bin/python /home/dimka/mos.ru/sad.py 
 /etc/init.d/xvfb stop
 
