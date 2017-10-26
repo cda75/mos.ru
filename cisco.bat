bcb15bff22a6ab578f3304fadbe52b1fc7a40bf8
cda75 <dchestnov@mail.ru>
2017-10-26 15:59:17 +0300

    cisco

--- cisco.bat
+++ cisco.bat
@@ -1,1 +1,9 @@
-C:\Python27\python F:\Info\Python\mos.ru\cisco.py
+@echo off
+F:
+cd  F:\Info\Python\mos.ru
+if "%1"=="" (
+	C:\Python27\python F:\Info\Python\mos.ru\cisco.py
+	) else (
+	C:\Python27\python F:\Info\Python\mos.ru\cisco.py -action %1
+	)
+exit
