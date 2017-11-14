0e9fb19bf7cc624134394b4a854ef8423a5080bf
cda75 <dchestnov@mail.ru>
2017-11-14 19:13:08 +0300

    bat file changed

--- check_dnevnik.sh
+++ check_dnevnik.sh
@@ -4,7 +4,7 @@ export DISPLAY=:99
 /etc/init.d/xvfb start
 echo "Xvfb started at DISPLAY 99"
 echo "Start checking diary......"
-/usr/bin/python /home/dimka/mos.ru/dnevnik.py -a hw >> dnevnik.log 
+/usr/bin/python /home/dimka/mos.ru/dnevnik.py -a update >> dnevnik.log 
 /etc/init.d/xvfb stop
 echo "Diary successfully checked"
 
